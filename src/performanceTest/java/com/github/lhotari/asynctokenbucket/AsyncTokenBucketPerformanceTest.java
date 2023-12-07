692b9004c0d3724c77295cb89a4b98f644d82920
Lari Hotari <lhotari@users.noreply.github.com>
2023-12-06 18:00:32 -0800

    Move performance test to separate source set

---
+++ src/performanceTest/java/com/github/lhotari/asynctokenbucket/AsyncTokenBucketPerformanceTest.java
@@ -0,0 +1,52 @@
+package com.github.lhotari.asynctokenbucket;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class AsyncTokenBucketPerformanceTest {
+    private AsyncTokenBucket asyncTokenBucket;
+
+    @Tag("performance")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 100})
+    void shouldPerformanceOfConsumeTokensBeSufficient(int numberOfThreads) throws InterruptedException {
+        long ratePerSecond = 100_000_000;
+        int durationSeconds = 10;
+        Thread[] threads = new Thread[numberOfThreads];
+        for (int i = 0; i < 2; i++) {
+            asyncTokenBucket = AsyncTokenBucket.builder()
+                    .rate(ratePerSecond)
+                    .initialTokens(2 * ratePerSecond)
+                    .capacity(2 * ratePerSecond)
+                    .build();
+            long startNanos = System.nanoTime();
+            long endNanos = startNanos + TimeUnit.SECONDS.toNanos(durationSeconds);
+            AtomicLong totalCounter = new AtomicLong();
+            System.out.printf("Consuming for %d seconds...%n", durationSeconds);
+            for (int t = 0; t < numberOfThreads; t++) {
+                Thread thread = new Thread(() -> {
+                    long counter = 0;
+                    while (System.nanoTime() < endNanos) {
+                        asyncTokenBucket.consumeTokens(1);
+                        counter++;
+                    }
+                    totalCounter.addAndGet(counter);
+                });
+                threads[t] = thread;
+                thread.start();
+            }
+            for (int t = 0; t < numberOfThreads; t++) {
+                threads[t].join();
+            }
+            long totalCount = totalCounter.get();
+            System.out.println("Counter value " + totalCount + " tokens:" + asyncTokenBucket.tokens(true));
+            System.out.printf(Locale.US, "Achieved rate: %,d ops per second with %d threads%n",
+                    totalCount / durationSeconds,
+                    numberOfThreads);
+        }
+    }
+}
--- src/test/java/com/github/lhotari/asynctokenbucket/AsyncTokenBucketTest.java
+++ src/test/java/com/github/lhotari/asynctokenbucket/AsyncTokenBucketTest.java
@@ -1,14 +1,10 @@
 package com.github.lhotari.asynctokenbucket;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.LongSupplier;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class AsyncTokenBucketTest {
     private final AtomicLong manualClockSource = new AtomicLong(TimeUnit.SECONDS.toNanos(100));
@@ -75,45 +71,5 @@ class AsyncTokenBucketTest {
         assertEquals(asyncTokenBucket.getTokens(), 1);
         incrementMillis(150);
         assertEquals(asyncTokenBucket.getTokens(), 3);
-    }
-
-    @Tag("performance")
-    @ParameterizedTest
-    @ValueSource(ints = {1, 10, 100})
-    void shouldPerformanceOfConsumeTokensBeSufficient(int numberOfThreads) throws InterruptedException {
-        long ratePerSecond = 100_000_000;
-        int durationSeconds = 10;
-        Thread[] threads = new Thread[numberOfThreads];
-        for (int i = 0; i < 2; i++) {
-            asyncTokenBucket = AsyncTokenBucket.builder()
-                    .rate(ratePerSecond)
-                    .initialTokens(2 * ratePerSecond)
-                    .capacity(2 * ratePerSecond)
-                    .build();
-            long startNanos = System.nanoTime();
-            long endNanos = startNanos + TimeUnit.SECONDS.toNanos(durationSeconds);
-            AtomicLong totalCounter = new AtomicLong();
-            System.out.printf("Consuming for %d seconds...%n", durationSeconds);
-            for (int t = 0; t < numberOfThreads; t++) {
-                Thread thread = new Thread(() -> {
-                    long counter = 0;
-                    while (System.nanoTime() < endNanos) {
-                        asyncTokenBucket.consumeTokens(1);
-                        counter++;
-                    }
-                    totalCounter.addAndGet(counter);
-                });
-                threads[t] = thread;
-                thread.start();
-            }
-            for (int t = 0; t < numberOfThreads; t++) {
-                threads[t].join();
-            }
-            long totalCount = totalCounter.get();
-            System.out.println("Counter value " + totalCount + " tokens:" + asyncTokenBucket.tokens(true));
-            System.out.printf(Locale.US, "Achieved rate: %,d ops per second with %d threads%n",
-                    totalCount / durationSeconds,
-                    numberOfThreads);
-        }
     }
 }
